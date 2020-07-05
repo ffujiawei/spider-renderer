5341a64eec112077a3f41b7d88b71799b32fb23d
White Turing <1748202178@qq.com>
2020-07-05 10:58:51 +0800

    示例与功能测试

---
+++ examples/http.py
@@ -0,0 +1,4 @@
+from renderer.utils import download_file
+
+src = 'http://www.meixian.gov.cn/uploadfile/file/20191206/20191206142742759.xls'
+download_file(src, 'examples/excel/data')
---
+++ examples/ph.py
@@ -0,0 +1,7 @@
+from renderer.utils import split_path
+
+path = 'c://dev/renderer/img.png'
+url = 'https://github.com/img.png'
+
+split_path(path)
+split_path(url)
