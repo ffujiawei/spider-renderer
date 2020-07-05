f5395555ba95f5e3952f17c3ad7b7e1d71380dea
White Turing <1748202178@qq.com>
2020-07-05 20:19:22 +0800

    Implements ModularLoader class that can freely combine outside the template.

--- renderer/__init__.py
+++ renderer/__init__.py
@@ -1,6 +1,7 @@
 '''Building a modular crawler template system based on Jinja2'''
 
 import re
+import warnings
 from datetime import datetime
 
 from jinja2 import Environment, FileSystemLoader
@@ -10,9 +11,56 @@ from .utils import (replace_all, replace_ws, retry_get, retry_post, rm_tags,
 from .utils.constants import DATETIME_12, WHITESPACE
 
 
+class ModularLoader(FileSystemLoader):
+    
+    def __init__(self, searchpath, encoding="utf-8", followlinks=False):
+        if not isinstance(searchpath, (tuple, set, list)):
+            searchpath = [searchpath]
+        self.searchpath = searchpath
+        self.encoding = encoding
+        self.followlinks = followlinks
+        self.templates = []
+        self.flag = False
+        self.head, self.body, self.foot = '', '', ''
+
+    def get_source(self, environment, template):
+        x, y, z = super().get_source(environment, template)
+        if self.flag:
+            return x, y, z
+        self.flag = True
+        return self.head+self.body+x+self.foot, y, z
+
+    def extends(self, templates):
+        if not isinstance(templates, (tuple, set, list)):
+            templates = (templates,)
+        ext = ''
+        for template in templates:
+            ext += '{% extends "template" %}\n'.replace('template', template)
+        self.head = ext + self.head
+
+    def include(self, templates, left=False):
+        if not isinstance(templates, (tuple, set, list)):
+            templates = (templates,)
+        ext = ''
+        for template in templates:
+            ext += '{% include "template" %}\n'.replace('template', template)
+        if left:
+            self.body += ext
+        else:
+            self.foot += ext
+
+    def include_left(self, templates):
+        self.include(templates, left=True)
+
+    def reset(self):
+        self.flag = False
+        self.head, self.body, self.foot = '', '', ''
+
+
 def render_templatefile(dst, templatefile, **kwargs):
-    env = Environment(loader=FileSystemLoader(
-        kwargs.pop('templates_folder', './templates')))
+    default_loader = FileSystemLoader(
+        kwargs.pop('templates_folder', './templates'))
+    env = Environment(loader=kwargs.pop('loader', default_loader))
     template = env.get_template(templatefile)
     with open(dst, 'w', encoding='utf-8') as fp:
         fp.write(template.render(**kwargs))
